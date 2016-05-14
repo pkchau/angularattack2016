2f26a7b3707646a1b64ccb19e2c764322b34e448
Peggy Chau <urbanescapist713@gmail.com>
2016-05-14 17:20:56 -0400

    still trying to make new page, have annotations error

--- src/app/app.component3.ts
+++ src/app/app.component3.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import '../../public/css/styles.css';
 @Component({
   selector: 'overview-page',
-  template: require('./app.component3.html'),
-  styles: [require('./app.component3.css')]
+  template: require('./app.component.html'),
+  styles: [require('./app.component.css')]
 })
 export class OverviewPage { }
