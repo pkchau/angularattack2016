4ec9793d5ad9d28618d4131c53be1a54920e4822
Peggy Chau <urbanescapist713@gmail.com>
2016-05-15 00:36:10 -0400

    working on packing list page (4)

---
+++ src/app/app.component4.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+@Component({
+  selector: 'packinglist-page',
+  template: require('./app.component4.html'),
+  styles: [require('./app.component4.css')]
+})
+export class PackingListPage { }
