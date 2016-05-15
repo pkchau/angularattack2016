d8cf1c1e551c8d345a9410e5352e40780a7b569e
Peggy Chau <urbanescapist713@gmail.com>
2016-05-15 12:41:42 -0400

    adding in account info page

---
+++ src/app/app.component_account.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+@Component({
+  selector: 'accountoverview-page',
+  template: require('./app.component_account.html'),
+  styles: [require('./app.component_account.css')]
+})
+export class AccountOverviewPage { }
