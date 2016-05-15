ff4d11d7fe2e982864f31144e0cb82c53bcae15d
Peggy Chau <urbanescapist713@gmail.com>
2016-05-15 18:51:36 -0400

    sign up page Aydar created, uploading for him since he had committed other changes to index.html and appcomponent2 which could mess with existing files on git

---
+++ src/app/app.componentSignup.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+@Component({
+  selector: 'componentsignup-page',
+  template: require('./app.componentSignup.html'),
+  styles: [require('./app.componentSignup.css')]
+})
+export class ComponentSignupPage { }
