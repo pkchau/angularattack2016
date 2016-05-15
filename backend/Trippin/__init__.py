8ce771ce84ba91e355195302d19d386ed651ff56
Rohan Puri <rohanpuri@Rohans-MBP.T-mobile.com>
2016-05-15 15:04:53 -0500

    adding in CORS

--- backend/Trippin/__init__.py
+++ backend/Trippin/__init__.py
@@ -2,9 +2,13 @@
 from flask import Flask
 from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.cors import CORS 
+
 
 app=Flask('Trippin')
+CORS(app)
 app.config.from_object('config')
+
 login_manager=LoginManager()
 login_manager.init_app(app)
 
