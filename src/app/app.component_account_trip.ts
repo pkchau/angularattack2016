8213e38e94b46d1b981609d4dfc2d94d5d5efafc
Peggy Chau <urbanescapist713@gmail.com>
2016-05-15 16:44:40 -0400

    adding in account page after user clicks on trip

---
+++ src/app/app.component_account_trip.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+@Component({
+  selector: 'accounttrip-page',
+  template: require('./app.component_account_trip.html'),
+  styles: [require('./app.component_account_trip.css')]
+})
+export class AccountTripPage { }
