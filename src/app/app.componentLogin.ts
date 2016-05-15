07d5ea798d7df582d434981c444f31836c7449b1
Aydar Fayzullin <aydar_fayzullin@yahoo.com>
2016-05-15 16:39:13 -0400

    LoginFiles

---
+++ src/app/app.componentLogin.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+@Component({
+  selector: 'componentlogin-page',
+  template: require('./app.componentLogin.html'),
+  styles: [require('./app.componentLogin.css')]
+})
+export class ComponentLoginPage { }
